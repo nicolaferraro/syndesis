2a73a852264d2c442adec0ade08d0460adf13b72
nferraro <ni.ferraro@gmail.com>
2019-03-13 10:28:12 +0100

    Adding templating customizer for Camel K

---
+++ app/server/controller/src/main/java/io/syndesis/server/controller/integration/camelk/customizer/TemplatingCamelKIntegrationCustomizer.java
@@ -0,0 +1,47 @@
+package io.syndesis.server.controller.integration.camelk.customizer;
+
+import io.syndesis.common.model.integration.IntegrationDeployment;
+import io.syndesis.common.model.integration.StepKind;
+import io.syndesis.common.model.integration.step.template.TemplateStepLanguage;
+import io.syndesis.server.controller.integration.camelk.crd.Integration;
+import io.syndesis.server.controller.integration.camelk.crd.IntegrationSpec;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static io.syndesis.common.util.Optionals.asStream;
+
+/**
+ * Adds libraries needed for templating steps
+ */
+@Component
+public class TemplatingCamelKIntegrationCustomizer implements CamelKIntegrationCustomizer {
+
+    @Override
+    public Integration customize(IntegrationDeployment deployment, Integration integration) {
+
+        Set<TemplateStepLanguage> languages = deployment.getSpec().getFlows().stream()
+            .flatMap(f -> f.getSteps().stream())
+            .filter(s -> s.getStepKind() == StepKind.template)
+            .flatMap(s -> asStream(Optional.of(s.getConfiguredProperties().get("language"))))
+            .map(TemplateStepLanguage::stepLanguage)
+            .collect(Collectors.toSet());
+
+        if (!languages.isEmpty()) {
+            IntegrationSpec.Builder spec = new IntegrationSpec.Builder();
+            if (integration.getSpec() != null) {
+                spec = spec.from(integration.getSpec());
+            }
+
+            for (TemplateStepLanguage lan : languages) {
+                spec = spec.addDependencies("mvn:" + lan.mavenDependency());
+            }
+
+            integration.setSpec(spec.build());
+        }
+        return integration;
+    }
+
+}
